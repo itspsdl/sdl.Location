247495cb4a5a1889a80df71b48ccb6b1273c052d
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2020-05-29 01:13:39 +0900

    Implement location requesting

--- app/src/main/java/jp/ac/titech/itpro/sdl/location/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/location/MainActivity.java
@@ -1,14 +1,142 @@
 package jp.ac.titech.itpro.sdl.location;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private final static String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private final static int REQ_PERMISSIONS = 1111;
+
+
+    private TextView infoView;
+
+    private GoogleApiClient apiClient;
+    private FusedLocationProviderClient locationClient;
+    private LocationRequest request;
+    private LocationCallback callback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoView = findViewById(R.id.info_view);
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        callback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    double lat = location.getLatitude();
+                    double lng = location.getLongitude();
+                    infoView.setText(getString(R.string.info_format, lat, lng));
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        apiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        apiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        startLocationUpdate(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspented");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
+    }
+
+    private void startLocationUpdate(boolean reqPermission) {
+        if (checkPermission(reqPermission)) {
+            locationClient.requestLocationUpdates(request, callback, null);
+        }
+    }
+
+    private boolean checkPermission(boolean reqPermission) {
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                if (reqPermission) {
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, REQ_PERMISSIONS);
+                } else {
+                    String text = getString(R.string.toast_requires_permission, permission);
+                    Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int reqCode, @NonNull String[] permissions, @NonNull int[] grants) {
+        Log.d(TAG, "onRequestPermissionsResult");
+        if (reqCode == REQ_PERMISSIONS) {
+            startLocationUpdate(false);
+        }
     }
 }
