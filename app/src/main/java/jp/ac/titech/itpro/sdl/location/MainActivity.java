fabb66bed7ce27142adfd78a572875f18c5b1045
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2021-05-07 11:33:46 +0900

    Initial import

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/location/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.titech.itpro.sdl.location;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
